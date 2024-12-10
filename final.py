603f6d9f539522bcd5dece36f4360ae6a96404b9
Muhammad Bin Aslam <92623180+muhammad2702@users.noreply.github.com>
2024-12-10 16:54:59 +0500

    Update final.py

--- final.py
+++ final.py
@@ -969,6 +969,7 @@ def preprocess_and_predict():
     os.makedirs('predictions', exist_ok=True)
     predictions_df.to_csv('predictions/latest_predictions.csv', index=False)
     print("Predictions saved to 'predictions/latest_predictions.csv'.")
+    print(predictions_df)
     predictions_dict = predictions_df.to_dict(orient="records")
     return { "status": "success", "predictions": predictions_dict }
 
